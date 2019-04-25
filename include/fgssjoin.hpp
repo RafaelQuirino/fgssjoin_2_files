d4d080c8bf102869b9e72e2cfe6c51edc9d65620
Rafael Quirino <rafael.david.quirino@gmail.com>
2019-04-25 08:28:45 -0300

    Adding files for the main fgssjoin interface.

---
+++ include/fgssjoin.hpp
@@ -0,0 +1,41 @@
+/* DOCUMENTATION
+
+*/
+
+#ifndef _FGSSJOIN_HPP_
+#define _FGSSJOIN_HPP_
+
+#define FGSSJOIN_CPU 0
+#define FGSSJOIN_GPU 1
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin_inverted_index ();
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin_filtering ();
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin_checking ();
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin ();
+
+
+
+#endif // _FGSSJOIN_HPP_
---
+++ src/fgssjoin.cpp
@@ -0,0 +1,41 @@
+#include "fgssjoin.hpp"
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin_inverted_index ()
+{
+
+}
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin_filtering ()
+{
+
+}
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin_checking ()
+{
+
+}
+
+
+
+/* DOCUMENTATION
+ *
+ */
+void fgssjoin ()
+{
+    
+}
